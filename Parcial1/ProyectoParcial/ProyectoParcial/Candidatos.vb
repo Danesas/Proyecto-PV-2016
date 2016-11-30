2158eb48fc6439b088ce13d569e8799712ba5078
Hernan Quinde <andres.ra22@gmail.com>
2016-11-30 12:43:32 -0500

    Creación Clase Candidatos

---
+++ Parcial1/ProyectoParcial/ProyectoParcial/Candidatos.vb
@@ -0,0 +1,33 @@
+﻿Public Class Candidatos
+
+    Private _usuario As String
+    Public Property Usuario() As String
+        Get
+            Return _usuario
+        End Get
+        Set(ByVal value As String)
+            _usuario = value
+        End Set
+    End Property
+
+    Private _clave As String
+    Public Property Clave() As String
+        Get
+            Return _clave
+        End Get
+        Set(ByVal value As String)
+            _clave = value
+        End Set
+    End Property
+
+    Private _dignidad As String
+    Public Property Dignidad() As String
+        Get
+            Return _dignidad
+        End Get
+        Set(ByVal value As String)
+            _dignidad = value
+        End Set
+    End Property
+
+End Class
