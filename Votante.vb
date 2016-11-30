b89eacad4f3269588ef540ecfc01029ffc0ee6d5
danieleve <daniel-eve1@hotmail.com>
2016-11-30 12:45:41 -0500

    Clase Votante

---
+++ Votante.vb
@@ -0,0 +1,13 @@
+﻿Public Class Votante
+    Inherits Persona
+
+    Private _cedula As String
+    Public Property Cedula() As String
+        Get
+            Return _cedula
+        End Get
+        Set(ByVal value As String)
+            _cedula = value
+        End Set
+    End Property
+End Class
