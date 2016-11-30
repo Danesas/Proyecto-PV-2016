bc98312cfdae241d7a516827bc4a252069242373
danieleve <daniel-eve1@hotmail.com>
2016-11-30 12:44:57 -0500

    Clase Administrador

---
+++ Parcial1/ProyectoParcial/ProyectoParcial/Administrador.vb
@@ -0,0 +1,23 @@
+﻿Public Class Administrador
+    Inherits Persona
+
+    Private _usuario As String
+    Public Property Usuario() As String
+        Get
+            Return _usuario
+        End Get
+        Set(ByVal value As String)
+            _usuario = value
+        End Set
+    End Property
+
+    Private clave As String
+    Public Property NewProperty() As String
+        Get
+            Return clave
+        End Get
+        Set(ByVal value As String)
+            clave = value
+        End Set
+    End Property
+End Class
