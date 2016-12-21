bdf5fadd332755873d7f00efb3d19012105ada40
Josty <josty-12@hotmail.com>
2016-12-21 10:56:51 -0500

    Clase Votante

---
+++ Parcial1/ProyectoParcial/ProyectoParcial/Votante.vb
@@ -0,0 +1,25 @@
+﻿Public Class Votante
+    Inherits Persona
+
+    Private _cedula As String
+    Public Property Cedula() As String
+        Get
+            Return _cedula
+        End Get
+        Set(ByVal value As String)
+            _cedula = value
+        End Set
+    End Property
+
+    Public Sub New(nombre As String, apellido As String, edad As Integer, cedula As String)
+        Me.Nombre = nombre
+        Me.Apellido = apellido
+        Me.Edad = edad
+        Me.Cedula = cedula
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return "nombre: " & Me.Nombre & " apellido: " & Me.Apellido & " edad: " & Me.Edad & " cedula: " & Me.Cedula
+    End Function
+
+End Class
