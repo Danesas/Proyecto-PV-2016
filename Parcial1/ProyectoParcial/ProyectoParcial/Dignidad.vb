92e3bb3ff4bb03f40fe90240ad21b4acaaa0d6d2
Josty <josty-12@hotmail.com>
2017-01-04 11:03:00 -0500

    Clase Dignidad

---
+++ Parcial1/ProyectoParcial/ProyectoParcial/Dignidad.vb
@@ -0,0 +1,21 @@
+﻿Public Class Dignidad
+
+    Private _nombreDig As String
+    Public Property NombreDig() As String
+        Get
+            Return _nombreDig
+        End Get
+        Set(ByVal value As String)
+            _nombreDig = value
+        End Set
+    End Property
+
+    Public Sub New(nombreDig As String)
+        Me.NombreDig = nombreDig
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return "nombre: " & Me.NombreDig
+    End Function
+
+End Class
