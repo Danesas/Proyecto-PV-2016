9d95e6e3ee42ee9364c0912d2b966b297703cc99
Josty <josty-12@hotmail.com>
2017-01-04 10:59:05 -0500

    Actualizar candidato

--- Parcial1/ProyectoParcial/ProyectoParcial/Candidatos.vb
+++ Parcial1/ProyectoParcial/ProyectoParcial/Candidatos.vb
@@ -29,13 +29,27 @@
             _dignidad = value
         End Set
     End Property
-Public Sub New(nombre As String, apellido As String, edad As Integer, usuario As String, clave As String, dignidad As String)
+    
+    Private _voto As Integer
+    Public Property Voto() As String
+        Get
+            Return _voto
+        End Get
+        Set(ByVal value As String)
+            _voto = value
+        End Set
+    End Property
+    
+    
+    Public Sub New(nombre As String, apellido As String, edad As Integer, usuario As String, clave As String, dignidad As String, voto As Integer, rol As String)
         Me.Nombre = nombre
         Me.Apellido = apellido
         Me.Edad = edad
         Me.Usuario = usuario
         Me.Clave = clave
         Me.Dignidad = dignidad
+        Me.Voto = voto
+        Me.Rol = rol
     End Sub
 
 
